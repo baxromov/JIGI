852c126248c84870e5a5fa42f24c8b00f4b3fcbf
SherzodAxadov2 <sherzodaxadov69@gmail.com>
2023-11-07 16:25:14 +0500

    fix: bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='jigi',
-    version='1.0.6',
+    version='1.0.7',
     description='JIGI',
     author='Baxromov Shahzodbek',
     author_email='baxromov.shahzodbek@gmail.com',
